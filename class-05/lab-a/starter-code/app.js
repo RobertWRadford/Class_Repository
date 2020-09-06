ad9d7b90a2438c85950e02a01940af99f215a631
Robert Radford <60657749+RobertWRadford@users.noreply.github.com>
2020-09-06 19:37:19 -0400

    Update app.js
    added code for problem 4

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -76,7 +76,14 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+	var a = 0;
+	for (var i = 0; i < summArr.length-1; i++){
+		a = sum(summArr[i], a);
+		var numString += summArr[i] + ',';
+	}
+	a = sum(summArr[summArr.length-1], a);
+	numString += summArr[summArr.length-1] + ' was passed in as an array of numbers, and ' + a + ' is their sum.';
+	return([a, numString]);
 }
 
 // Here is the test for sumArray(); uncomment it to run it
